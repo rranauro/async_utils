8f112e0f2d6f0520253f11476c9c479dbb24e2ff
rranauro <ron@ranauro.net>
2019-01-11 16:35:34 -0500

    util.request

--- index.js
+++ index.js
@@ -104,9 +104,9 @@ var _request = function(settings, opts) {
 				url: url,
 				method: method,
 				json: true,
-				body: options.body,
+				body: options && options.body,
 				auth: opts.auth,
-				headers: options.headers || opts.headers
+				headers: options && options.headers || opts.headers
 			}), function(err, response) {
 				if (err || response.statusCode > 399) return callback(err || response.statusCode, err || response);
 				callback(null, opts.parseXML ? toJson.parseXML( response.body ) : response.body);			
